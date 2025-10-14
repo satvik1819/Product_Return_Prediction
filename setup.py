e0bb536fbc4ee8f61072498f7f1dcc9925f2be4d
satvik1819 <nagasathvik19@gmail.com>
2025-10-14 09:45:41 +0530

    The setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements(file_path: str) -> List[str]:
-    """Reads requirements from the given file and returns them as a list."""
-    requirements = []
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.strip() for req in requirements if req.strip() != ""]
-
-        if '-e .' in requirements:
-            requirements.remove('-e .')
-
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
     return requirements
 
 setup(
