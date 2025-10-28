e20740035fd794a0578ee662b12887dea850eee4
satvik1819 <nagasathvik19@gmail.com>
2025-10-28 10:09:19 +0530

    Added artifacts and fixed data_ingestion + exception file names

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,54 +4,44 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 
-
 @dataclass        #this is a decorator
 class DataIngestionConfig:
-    train_data_path: str=os.path.join("artifacts","train.csv") #these are the inputs we are givoing to data ingesting
-    test_data_path: str=os.path.join("artifacts","test.csv")    #and it will save it in artifacts folder
+    train_data_path: str=os.path.join("artifacts","train.csv") #these are the inputs we are givoing to data ingesting 
+    test_data_path: str=os.path.join("artifacts","test.csv")    #and it will save it in artifacts folder 
     raw_data_path: str=os.path.join("artifacts","data.csv")
-
-
 
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
-
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv("notebook\data\stud.csv")       #here we can read from anywhere mongodb or any other data source
+            import os
+            df = pd.read_csv(os.path.join("notebook", "data", "stud.csv"))
+#here we can read from anywhere mongodb or any other data source
             logging.info("Read the dataset as dataframe")
-
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) #saving in this data path
-
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info("Ingestion of the data is completed")
-
 
             return (
                 self.ingestion_config.train_data_path,
@@ -59,14 +49,18 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-       
+        
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-   
+    
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
+
+
+
+        
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,3 +50,62 @@ class ModelTrainer:
                 "Random Forest":RandomForestRegressor(),
                 "AdaBoost Regressor":AdaBoostRegressor(),
                 "Gradient Boosting":GradientBoostingRegressor()
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+               
+            }
+
+
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)  #this function we have creayted in utils
+
+
+            #to get best model score from dict
+            best_model_score=max(sorted(model_report.values()))
+
+
+            #to get best model name from dict
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model=models[best_model_name]
+
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best model found on training and testing dataset")
+           
+            #saving best model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(x_test)
+            r2_square=r2_score(y_test,predicted)
+            return r2_square
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
